604bee8540787179eeb22448956707e4c385e010
Chipp Jansen <chipp.jansen@rca.ac.uk>
2024-11-27 16:41:07 +0000

    update train and eval scripts

---
+++ sml_eval/eval_sml_lab_test.sh
@@ -0,0 +1,9 @@
+# NOTE NEED SET --resume TO THE CORRECT PATH
+
+# single image model eval for Deep TEN
+python train.py --dataset sml_lab_test --model resnet50 --mode single --eval --checkname single_deepten_resnet50 --batch-size 64  --resume runs/sml_lab_dual/resnet50/dual_image_model/model_best.pth.tar 
+
+# DUAL image model training for Deep TEN using resnet50 as backbone
+#python train.py --dataset sml_lab_test --model resnet50 --mode dual --eval --checkname dual_deepten_resnet50 --batch-size 64  --resume runs/sml_lab_dual/resnet50/dual_image_model/model_best.pth.tar 
+
+# --resume runs/sml_lab_dual/resnet50/dual_image_model/model_best.pth.tar 
--- sml_eval/train_sml_lab.sh
+++ sml_eval/train_sml_lab.sh
@@ -1,16 +1,8 @@
- # ("sml_lab", "single", "test", 1000, transform_val),
-# ("sml_lab_test", "single", None, None, transform_val),
-# ("sml_expo_eval", "single", None, None, transform_val),
-# ("sml_lab", "dual", "train", 1000, transform_train),
-# ("sml_lab", "dual", "test", 1000, transform_val),
-# ("sml_lab_test", "dual", None, None, transform_val),
-# ("sml_expo_eval", "dual", None, None, transform_val)
+# single image model training for Deep TEN using resnet50 as backbone
+python train.py --dataset sml_lab --model resnet50 --mode single --lr-scheduler cos --epochs 30 --checkname single_deepten_resnet50 --lr 0.025 --batch-size 64 --train-n-per-class 1000 --test-n-per-class 1000 --plot
 
+# DUAL image model training for Deep TEN using resnet50 as backbone
+#python train.py --dataset sml_lab --model resnet50 --mode dual --lr-scheduler cos --epochs 30 --checkname dual_deepten_resnet50 --lr 0.025 --batch-size 64 --train-n-per-class 1000 --test-n-per-class 1000 --plot
 
-
-
-# python3 train_dual.py --model resnet50 --dataset sml_lab_dual --checkname dual_image_model --batch-size 512 --test-batch-size 512 --lr 0.1 --epochs 30 --train-n-per-class 1000 --pairing-strategy product --include-inverse --resume runs/sml_lab_dual/resnet50/dual_image_model/model_best.pth.tar --plot
-
-
-# baseline
-#python train_dist.py --dataset imagenet --model resnet50 --lr-scheduler cos --epochs 120 --checkname resnet50_check --lr 0.025 --batch-size 64
+# use resume to resume training
+# --resume runs/sml_lab_dual/resnet50/dual_image_model/model_best.pth.tar 
